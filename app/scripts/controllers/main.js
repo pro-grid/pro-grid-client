b3c4cbe5ce6ed1fe7f5f85f042e97570328beddf
Austin Pray <austin@austinpray.com>
2014-06-27 13:17:39 -0500

    removes usercolor
    see
    https://github.com/pro-grid/pro-grid-client/issues/4
    https://github.com/pro-grid/pro-grid/issues/111

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,20 +16,6 @@ angular.module('proGridApp')
       }
     };
 
-    var colorToHex = function (color) {
-      if (color.substr(0, 1) === '#') {
-        return color;
-      }
-      var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
-
-      var red = parseInt(digits[2]);
-      var green = parseInt(digits[3]);
-      var blue = parseInt(digits[4]);
-
-      var rgb = blue | (green << 8) | (red << 16);
-      return digits[1] + '#' + rgb.toString(16);
-    };
-
     socket.on('server ready', function (data) {
       //grid is an array
       console.log('Hello There! Hope you are enjoying the app. Please be nice! Please help us fix our issues over at: https://github.com/pro-grid/pro-grid Thank you. -progrid.io');
@@ -72,7 +58,7 @@ angular.module('proGridApp')
     $scope.gridRightClicked = function(row, col) {
       var selector = '.col_' + row + '_' + col;
       var el = document.querySelector(selector);
-      userColor = colorToHex(el.style.backgroundColor);
+      userColor = el.style.backgroundColor;
     };
 
     $scope.closeMessage = function() {
