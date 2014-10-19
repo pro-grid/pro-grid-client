b46ee8cbb38347750fb2d43435feaa44aa39ed17
Darren Cattle <cattle.darren@gmail.com>
2014-10-19 03:18:17 -0500

    Mod Patch
    Adding support for Heat Mod, Black Mod, Blend Mod. Secondary array support on client side to hold data and manipulate doms.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,66 @@
 'use strict';
 
 angular.module('proGridApp')
-  .controller('MainCtrl', ['$scope', 'Randomcolor', 'hostname', function ($scope, Randomcolor, hostname) {
+  .controller('MainCtrl', ['$scope', '$timeout', 'hostname', 'injectStyle', function ($scope, $timeout, hostname, injectStyle) {
     var socket = io.connect(hostname);
     // Socket listener for updating grid
-    var userColor = Randomcolor.new();
+    $scope.userColor = '';
+    $scope.mod = '0'; //0: no mod, 1: heat mod, 2: black mod, 3: blend mod
+    $scope.maxclicks = 0; //maxclicks
+    $scope.localdata = [];
+    for (var a = 0; a<data.gridArray.length-1; a++) {
+      localdata.push(new Array(data.gridArray.length));
+      for (var b = 0; b<data.gridArray.length-1; b++) {
+        localdata[a][b] = {row: a, col: b, color: '', clicks: 0};
+      }
+    }
+    $scope.user = {
+      color: '',
+      name: 'anon'
+    };
+    $scope.meta = {
+      numUsers: 0
+    };
+
     var apiKey = 1;
     var updateGrid = function(row, col, color) {
-      var selector = '.col_' + row + '_' + col;
-      var el = document.querySelector(selector);
-      if(el.style.backgroundColor) {
-        el.style.backgroundColor = '';
-      } else {
-        el.style.backgroundColor = color;
-      }
+      $timeout(function () {
+        localdata[row][col].color = color;
+        switch($scope.mod) {
+          case '0': $scope.gridArray[row][col].color = !!$scope.gridArray[row][col].color ? '' : color;
+          break;
+          case '1': $scope.gridArray[row][col].color = "#" + 
+          Math.floor(255*localdata[row][col].clicks/$scope.maxclicks).toString(16) + "0000";
+          break;
+          case '2': $scope.gridArray[row][col].color = !!$scope.gridArray[row][col].color ? '#000000' : '';
+          break;
+          case '3':
+          break;
+        }
+      }, 0);
     };
+
     socket.on('server ready', function (data) {
       //grid is an array
-      console.log('Hello There! Hope you are enjoying the app. Please be nice! Please help us fix our issues over at: https://github.com/pro-grid/pro-grid Thank you. -progrid.io');
-      data.gridArray.forEach(function (element) {
-        element.forEach(function (element) {
-          updateGrid(element.row, element.col, element.color);
-        });
-      });
+      var message = 'Hello There! Hope you are enjoying the app. Please be '+
+      'nice! Please help us fix our issues over at: '+
+      'https://github.com/pro-grid/pro-grid Thank you. -progrid.io';
+      console.log(message);
+      $scope.user.color = data.userColor;
+      injectStyle.gridDimensions(data.gridArray.length);
+      $timeout(function () {
+        $scope.gridArray = angular.copy(data.gridArray);
+      }, 0);
     });
 
     socket.on('fresh api key', function (data) {
       apiKey = data.apiKey;
+    });
+
+    socket.on('join', function (data) {
+      $scope.meta.numUsers = data;
+      $scope.$apply();
+      console.log(data);
     });
 
     socket.on('update', function (data) {
@@ -43,42 +77,66 @@ angular.module('proGridApp')
       console.log('goodbye');
     });
 
-    $scope.dimensions = 32;
-
-    $scope.generateGrid = function(num) {
-      return new Array(num);
-    };
-
     $scope.gridClicked = function(row, col) {
-      updateGrid(row, col, userColor);
-      socket.emit('clicked', { row: row, col: col, color: userColor, apiKey: apiKey });
+      updateGrid(row, col, $scope.user.color);
+      socket.emit('clicked', { row: row, col: col, color: $scope.user.color, apiKey: apiKey });
     };
 
     $scope.gridRightClicked = function(row, col) {
       var selector = '.col_' + row + '_' + col;
       var el = document.querySelector(selector);
-      userColor = colorToHex(el.style.backgroundColor);
-    };
-    
-    $scope.heatMod = function() {
-      var list = [];
-      for(var a = 0; a < data.gridArray.length; a++) {
-        for(var b = 0; b < data.gridArray.length; b++) {
-          var clicks = Math.random(100);
-          var maxclicks = 100;
-          var hex = Math.floor(255*clicks/maxclicks).toString(16);
-          var heat = "#" + hex + "0000";
-          list.push({"row": a,"col": b,"color": heat});
-        }
-      }
-      for(var c = 0; c < list.length-1; c++)
-        updateGrid(list[c].row, list[c].col, list[c].color);
+      $scope.user.color = el.style.backgroundColor;
     };
 
-    $scope.modOff = function() {
+    $scope.heatMod = function() {
+      $scope.mod = '1';
       for(var a = 0; a < data.gridArray.length; a++) {
         for(var b = 0; b < data.gridArray.length; b++) {
-          updateGrid(b, a, $scope.gridArray[b][a].color);
+          $scope.maxclicks = localdata[a][b] > $scope.maxclicks ? localdata[a][b]: $scope.maxclicks;}}
+      for(var a = 0; a < data.gridArray.length; a++) {
+        for(var b = 0; b < data.gridArray.length; b++) {
+          var hex = Math.floor(255*localdata[a][b].clicks/$scope.maxclicks).toString(16);
+          var heat = "#" + hex + "0000";
+          $scope.gridArray[a][b].color = heat;
+        }
+      }  
+    };
+
+    $scope.blackMod = function () {
+      $scope.mod = '2';
+      for(var a = 0; a < data.gridArray.length; a++) {
+        for(var b = 0; b < data.gridArray.length; b++) {
+          $scope.gridArray[a][b].color = !!$scope.gridArray[a][b].color ? '#000000' : '';
+        }
+      }
+    }
+
+    $scope.blendMod = function () {
+      $scope.mod = '3';
+      var list = [];
+      for(var a = 0; a < data.gridArray.length; a++) {
+        list.push({"r": 0,"g": 0,"b": 0});
+        for(var b = 0; b < data.gridArray.length; b++) {
+          list[a].r += parseInt(localdata[a][b].color.substring(1,3),16);
+          list[a].g += parseInt(localdata[a][b].color.substring(3,5),16);
+          list[a].b += parseInt(localdata[a][b].color.substring(5,7),16);
+        }
+      }
+      for(var a = 0; a < data.gridArray.length; a++) {
+        for(var b = 0; b < data.gridArray.length; b++) {
+          var cr = list[a].r*b/data.gridArray.length;
+          var cg = list[a].g*b/data.gridArray.length;
+          var cb = list[a].b*b/data.gridArray.length;
+          $scope.gridArray[a][b].color = "#" + Math.floor(cr).toString(16) + Math.floor(cg).toString(16) + Math.floor(cb).toString(16);
+        }
+      }
+    }
+
+    $scope.modOff = function() {
+      $scope.mod = '0';
+      for(var a = 0; a < data.gridArray.length; a++) {
+        for(var b = 0; b < data.gridArray.length; b++) {
+          $scope.gridArray[a][b].color = localdata[a][b].color;
         }
       }
     };
@@ -87,31 +145,19 @@ angular.module('proGridApp')
       $scope.message = false;
     };
     $scope.gridClicked = _.throttle($scope.gridClicked, 100, {trailing: false});
+
+
   }]);
 
 angular.module('proGridApp')
   .directive('ngRightClick', function($parse) {
-      return function(scope, element, attrs) {
-          var fn = $parse(attrs.ngRightClick);
-          element.bind('contextmenu', function(event) {
-              scope.$apply(function() {
-                  event.preventDefault();
-                  fn(scope, {$event:event});
-                });
-            });
-        };
-    });
-
-function colorToHex(color) {
-  if (color.substr(0, 1) === '#') {
-    return color;
-  }
-  var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
-
-  var red = parseInt(digits[2]);
-  var green = parseInt(digits[3]);
-  var blue = parseInt(digits[4]);
-
-  var rgb = blue | (green << 8) | (red << 16);
-  return digits[1] + '#' + rgb.toString(16);
-}
+    return function(scope, element, attrs) {
+      var fn = $parse(attrs.ngRightClick);
+      element.bind('contextmenu', function(event) {
+        scope.$apply(function() {
+          event.preventDefault();
+          fn(scope, {$event:event});
+        });
+      });
+    };
+  });
